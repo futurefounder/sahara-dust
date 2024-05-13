5dacebd105190e6349cdea5e57687db9620414a6
Jesse <jsk@posteo.de>
2024-05-13 17:52:41 +0200

    fix type errors

--- app/components/Quotes.tsx
+++ app/components/Quotes.tsx
@@ -6,16 +6,15 @@ export default function Quotes() {
           <div className="p-4 mb-6 lg:w-1/3 lg:mb-0">
             <div className="h-full text-center">
               <p className="italic leading-relaxed">
-                "Saharan dust can be harmful to your health.{" "}
+                &quot;Saharan dust can be harmful to your health.&quot;
                 <span className="font-bold">
-                  {" "}
-                  The particles can be breathed in and enter your lungs and
-                  blood stream,
-                </span>{" "}
+                  &quot; The particles can be breathed in and enter your lungs
+                  and blood stream,
+                </span>
                 potentially triggering asthma attacks in people who have asthma
                 and aggravating other respiratory conditions. Saharan dust
                 worsens air quality and increases the levels of particulate
-                matter in the air."
+                matter in the air.&quot;
               </p>
               <span className="inline-block w-10 h-1 mt-6 mb-4 rounded bg-amber-500"></span>
               <p className="text-gray-100">
@@ -27,8 +26,9 @@ export default function Quotes() {
           <div className="p-4 mb-6 lg:w-1/3 lg:mb-0">
             <div className="h-full text-center">
               <p className="leading-relaxed">
-                "Sharp increase of Saharan dust intrusions over the Western
-                Mediterranean and Euro-Atlantic region in winters 2020–2022."
+                &quot;Sharp increase of Saharan dust intrusions over the Western
+                Mediterranean and Euro-Atlantic region in winters
+                2020–2022.&quot;
               </p>
               <span className="inline-block w-10 h-1 mt-6 mb-4 rounded bg-amber-500"></span>
               <p className="text-gray-100">
@@ -42,11 +42,11 @@ export default function Quotes() {
           <div className="p-4 lg:w-1/3 lg:mb-0">
             <div className="h-full text-center">
               <p className="italic leading-relaxed">
-                "Our results provide evidence of: (1) the role of Saharan Dust
-                as an independent risk factor for adverse respiratory effect in
-                The Canary Islands; (2) the existence of a short-term
+                &quot;Our results provide evidence of: (1) the role of Saharan
+                Dust as an independent risk factor for adverse respiratory
+                effect in The Canary Islands; (2) the existence of a short-term
                 association between Sahara Dust Days and the risk of emergency
-                hospital admissions, that spreads over successive days."
+                hospital admissions, that spreads over successive days.&quot;
               </p>
               <span className="inline-block w-10 h-1 mt-6 mb-4 rounded bg-amber-500"></span>
               <p className="text-gray-100">Elena López–Villarrubia et al.</p>
