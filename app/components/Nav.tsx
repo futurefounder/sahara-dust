3714b5da1272c52f81fb93377006565086a3023b
Jesse <jsk@posteo.de>
2024-05-20 13:25:50 +0200

    fix linting error

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -66,7 +66,7 @@ export default function Navigation() {
                     viewBox="0 0 24 24"
                     stroke-width="1.5"
                     stroke="currentColor"
-                    class="w-6 h-6 mr-1"
+                    className="w-6 h-6 mr-1"
                   >
                     <path
                       stroke-linecap="round"
